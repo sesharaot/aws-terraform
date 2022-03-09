a1f5a0f37dfd5e736d078b1ab63eb1cc088a0e5c
sesharaot <tata.sesharao@gmail.com>
2022-03-09 20:35:16 +0530

    new code added

--- variable.tf
+++ variable.tf
@@ -2,12 +2,3 @@ variable "region" {
   default = "us-east-1"
 }
 
-variable "ami_id" {
-  type = map(string)
-
-  default = {
-    us-east-1    = "ami-011939b19c6bd1492"
-    eu-west-2    = "ami-132b3c7efe6sdfdsfd"
-    eu-central-1 = "ami-9787h5h6nsn"
-  }
-}
