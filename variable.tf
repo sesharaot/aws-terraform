640db1aa0f93572c0d02323290375acf9dd9dc2f
sesharaot <tata.sesharao@gmail.com>
2022-03-09 18:27:01 +0530

    ami modify

--- variable.tf
+++ variable.tf
@@ -6,7 +6,7 @@ variable "ami_id" {
   type = "map"
 
   default = {
-    us-east-1    = "ami-035b3c7efe6d061d5"
+    us-east-1    = "ami-011939b19c6bd1492"
     eu-west-2    = "ami-132b3c7efe6sdfdsfd"
     eu-central-1 = "ami-9787h5h6nsn"
   }
