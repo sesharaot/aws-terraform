d57e93fcb050dbc7137bf95832b0ae2a1d3f7f8f
sesharaot <tata.sesharao@gmail.com>
2022-03-10 09:05:40 +0530

    added sg

--- main.tf
+++ main.tf
@@ -7,11 +7,12 @@ resource "aws_instance" "web" {
   ami           = "ami-011939b19c6bd1492"
   instance_type = "t2.micro"
   key_name = "master"
-  
+  vpc_security_group_ids = "sg-0ea6ac3a80c3d7e36" 
   user_data = <<EOF
   #!/bin/bash  
+  sudo yum install wget -y
   sudo wget https://packages.chef.io/files/stable/chef-workstation/21.10.640/el/8/chef-workstation-21.10.640-1.el8.x86_64.rpm
-  sudo yum localinstall chef-workstation-21.10.640-1.el8.x86_64.rpm
+  sudo yum localinstall chef-workstation-21.10.640-1.el8.x86_64.rpm -y
   sudo chef -v
 EOF
   tags = {
