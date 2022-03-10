1d6b6890f76cda2ede0bfa6ad0f36f8cef51b0e8
sesharaot <tata.sesharao@gmail.com>
2022-03-10 12:03:24 +0530

    modified outvalues

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
-output "public_dns" {
-  description = "The public DNS name assigned to the instance. For EC2-VPC, this is only available if you've enabled DNS hostnames for your VPC"
-  value       = try(aws_instance.this[0].public_dns, aws_spot_instance_request.this[0].public_dns, "")
+output "id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.web.id
 }
 
 output "public_ip" {
-  description = "The public IP address assigned to the instance, if applicable. NOTE: If you are using an aws_eip with your instance, you should refer to the EIP's address directly and not use `public_ip` as this field will change after the EIP is attached"
-  value       = try(aws_instance.this[0].public_ip, aws_spot_instance_request.this[0].public_ip, "")
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.web.public_ip
 }
