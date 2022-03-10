d619bffdffd04c28453fd1c818d6bc3ba62ce79a
sesharaot <tata.sesharao@gmail.com>
2022-03-10 09:08:09 +0530

    modified sg

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "web" {
   ami           = "ami-011939b19c6bd1492"
   instance_type = "t2.micro"
   key_name = "master"
-  vpc_security_group_ids = "sg-0ea6ac3a80c3d7e36" 
+  vpc_security_group_ids = ["sg-0ea6ac3a80c3d7e36"] 
   user_data = <<EOF
   #!/bin/bash  
   sudo yum install wget -y
