e5f964a8b47201eab3eac8d1f4c41d123737e7de
sesharaot <tata.sesharao@gmail.com>
2022-03-10 08:26:22 +0530

    added user data

--- main.tf
+++ main.tf
@@ -6,7 +6,13 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami           = "ami-011939b19c6bd1492"
   instance_type = "t3.micro"
-
+  
+  user_data = <<EOF
+  #!/bin/bash  
+  sudo wget https://packages.chef.io/files/stable/chef-workstation/21.10.640/el/8/chef-workstation-21.10.640-1.el8.x86_64.rpm
+  sudo yum localinstall chef-workstation-21.10.640-1.el8.x86_64.rpm
+  sudo chef -v
+EOF
   tags = {
     Name = "TEST"
     role = "web"
